497bf56123467bea0baba22da8cd97463282bcdf
murimolda <murimolda@gmail.com>
2022-02-08 20:29:48 +0300

    feat: add API load function

--- index.js
+++ index.js
@@ -40,6 +40,46 @@ document.addEventListener("DOMContentLoaded", function () {
     //     }
     // })
 
+    const searchForm = document.querySelector('.header-search-input');
+    const searchIcon = document.querySelector('.header-search-icon');
+    const imageWrap = document.querySelector('.image-container')
+
+    const apiRequest = () => {
+        imageWrap.textContent = "";
+        const url = `https://api.unsplash.com/search/photos?query=${searchForm.value}&per_page=20&client_id=44p5ICsqB7rV99h5gLkFVA_fc4PTLn7YKs4j9RD8MGE`;
+        fetch(url)
+            .then(response => {
+                if (!response.ok) throw Error(response.statusText);
+                return response.json();
+            })
+            .then(data => {
+                loadImages(data);
+            })
+            .catch(error => console.log(error));
+    }
+
+    const loadImages = (data) => {
+        for (let i = 0; i < data.results.length; i++) {
+            let image = document.createElement("div");
+            image.className = "image-wrap";
+            image.style.backgroundImage = `url(${data.results[i].urls.raw}&w=1366&h=768)`;
+            image.addEventListener("dblclick", function () {
+                window.open(data.results[i].links.download, '_blank');
+            })
+            imageWrap.appendChild(image);
+        }
+    }
+
+    searchForm.addEventListener("keydown", (event) => {
+        if (event.key == "Enter")
+            apiRequest();
+    });
+
+    searchIcon.addEventListener("click", () => {
+        apiRequest();
+    });
+
+
 
 });
 
