6536622e73cc0556061ed8c4e8ebea88cdbdddcb
murimolda <murimolda@gmail.com>
2022-02-09 17:59:05 +0300

    feat: add day/night function

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 document.addEventListener("DOMContentLoaded", function () {
 
 
-    /*Function to find the element that is wider than the body of the page*/
+    /*функция показывает в консоли элементы, 
+    которые выходят за границы основного блока, 
+    и тогда появляется горизонтальная полоса прокрутки*/
     const docWidth = document.documentElement.offsetWidth;
 
     [].forEach.call(
@@ -13,36 +15,11 @@ document.addEventListener("DOMContentLoaded", function () {
         }
     );
 
-    // /*Change color theme*/
-    // const htmlColor = document.querySelector('[data-color]');
-    // const changeThemeButton = document.querySelector('.header-change-theme-button')
-
-    // const changeColorTheme = (theme) => {
-    //     const whiteThemeElements = document.querySelectorAll('[data-theme]');
-    //     if (theme === 'white') {
-    //         whiteThemeElements.forEach(element => {
-    //             element.classList.add("white-theme");
-    //             htmlColor.dataset.color = 'white'
-    //         });
-    //     } else if (theme === 'dark') {
-    //         whiteThemeElements.forEach(element => {
-    //             element.classList.remove("white-theme");
-    //             htmlColor.dataset.color = 'dark'
-    //         });
-    //     }
-    //     themeColor = theme
-    // }
-    // changeThemeButton.addEventListener('click', () => {
-    //     if (htmlColor.dataset.color === 'dark') {
-    //         changeColorTheme('white');
-    //     } else if (htmlColor.dataset.color === 'white') {
-    //         changeColorTheme('dark');
-    //     }
-    // })
 
     const searchForm = document.querySelector('.header-search-input');
     const searchIcon = document.querySelector('.header-search-icon');
-    const imageWrap = document.querySelector('.image-container')
+    const imageWrap = document.querySelector('.image-container');
+
 
     const apiRequest = () => {
         imageWrap.textContent = "";
@@ -57,6 +34,34 @@ document.addEventListener("DOMContentLoaded", function () {
             })
             .catch(error => console.log(error));
     }
+    /*Загрузка темы "горы" при открытии страницы. Закостылила, конечно, но пока не разобралась, как сделать изящней*/
+    window.addEventListener('load', () => {
+        imageWrap.textContent = "";
+        const url = `https://api.unsplash.com/search/photos?query=mountains&per_page=20&client_id=44p5ICsqB7rV99h5gLkFVA_fc4PTLn7YKs4j9RD8MGE`;
+        fetch(url)
+            .then(response => {
+                if (!response.ok) throw Error(response.statusText);
+                return response.json();
+            })
+            .then(data => {
+                loadImages(data);
+            })
+            .catch(error => console.log(error));
+    });
+
+    // /*fullscreen галерея*/
+    // const fullscreenContainer = document.querySelector('.fullscreen-container');
+
+    // const fullscreenGallery = (data) => {
+    //     console.log('+');
+    //     for (let i = 0; i < data.results.length; i++) {
+    //         let image = document.createElement("div");
+    //         image.className = "image-wrap";
+    //         image.style.backgroundImage = `url(${data.results[i].urls.raw}&w=1366&h=768)`;
+    //         fullscreenContainer.appendChild(image);
+    //     }
+    // }
+
 
     const loadImages = (data) => {
         for (let i = 0; i < data.results.length; i++) {
@@ -64,20 +69,41 @@ document.addEventListener("DOMContentLoaded", function () {
             image.className = "image-wrap";
             image.style.backgroundImage = `url(${data.results[i].urls.raw}&w=1366&h=768)`;
             image.addEventListener("dblclick", function () {
-                window.open(data.results[i].links.download, '_blank');
-            })
+                console.log('+');
+            });
             imageWrap.appendChild(image);
         }
     }
 
-    searchForm.addEventListener("keydown", (event) => {
-        if (event.key == "Enter")
+    /*загрузка изображений по клику Enter*/
+    searchForm.addEventListener("keyup", (event) => {
+        if (event.key === "Enter")
             apiRequest();
     });
 
+    /*загрузка изображений по клику на иконку поиска*/
     searchIcon.addEventListener("click", () => {
         apiRequest();
     });
+
+    /*Функция День/Ночь, меняет оформление страницы в зависимости от времени суток*/
+    const changeTheme = () => {
+        let date = new Date();
+        let hour = date.getHours();
+        const whiteThemeElements = document.querySelectorAll('[data-theme]');
+        if (hour >= 7 && hour < 19) {
+            whiteThemeElements.forEach(element => {
+                element.classList.add("white-theme");
+            });
+        } else {
+            whiteThemeElements.forEach(element => {
+                element.classList.remove("white-theme");
+            });
+        }
+    }
+
+    window.addEventListener('load', changeTheme);
+
 
 
 
